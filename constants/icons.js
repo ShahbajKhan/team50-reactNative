425d91a8b51ccb13e9ad337553a8f18d4903f824
ShohanShabbir <c171011@ugrad.iiuc.ac.bm>
2021-06-04 17:18:06 +0600

    Home Screen

---
+++ constants/icons.js
@@ -0,0 +1,73 @@
+export const back = require("../assets/icons/back.png");
+export const car = require("../assets/icons/car.png");
+export const cutlery = require("../assets/icons/cutlery.png");
+export const donut = require("../assets/icons/donut.png");
+export const drink = require("../assets/icons/drink.png");
+export const fire = require("../assets/icons/fire.png");
+export const fries = require("../assets/icons/fries.png");
+export const hamburger = require("../assets/icons/hamburger.png");
+export const hotdog = require("../assets/icons/hotdog.png");
+export const like = require("../assets/icons/like.png");
+export const list = require("../assets/icons/list.png");
+export const location = require("../assets/icons/location.png");
+export const master_card = require("../assets/icons/mastercard.png");
+export const nearby = require("../assets/icons/nearby.png");
+export const noodle = require("../assets/icons/noodle.png");
+export const pin = require("../assets/icons/pin.png");
+export const pizza = require("../assets/icons/pizza.png");
+export const red_pin = require("../assets/icons/red-pin.png");
+export const rice_bowl = require("../assets/icons/rice-bowl.png");
+export const salad = require("../assets/icons/salad.png");
+export const search = require("../assets/icons/search.png");
+export const basket = require("../assets/icons/shopping-basket.png");
+export const star = require("../assets/icons/star.png");
+export const sushi = require("../assets/icons/sushi.png");
+export const user = require("../assets/icons/user.png");
+export const tree = require("../assets/icons/tree.png");
+export const bush = require("../assets/icons/bush.png");
+export const fruit = require("../assets/icons/fruits.png");
+export const rose = require('../assets/icons/rose.png');
+export const floral = require('../assets/icons/floral.png');
+export const gift = require('../assets/icons/plant.png');
+export const patio = require('../assets/icons/living-room.png');
+export const blubs = require('../assets/icons/coffee-plant.png');
+export const indoor = require('../assets/icons/indoor-plant.png');
+
+
+
+export default {
+    back,
+    car,
+    cutlery,
+    donut,
+    drink,
+    fire,
+    fries,
+    hamburger,
+    hotdog,
+    like,
+    list,
+    location,
+    master_card,
+    nearby,
+    noodle,
+    pin,
+    pizza,
+    red_pin,
+    rice_bowl,
+    salad,
+    search,
+    basket,
+    star,
+    sushi,
+    user,
+    tree,
+    bush,
+    fruit,
+    rose,
+    floral,
+    gift,
+    patio,
+    blubs,
+    indoor,
+}
